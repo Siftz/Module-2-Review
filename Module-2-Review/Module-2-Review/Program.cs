1210cf722c5473cc7efd41a9c0320f67a83ce63e
Logan Akins <lakins2@gmail.com>
2025-04-23 20:42:33 -0400

    Student List Commit

--- Module-2-Review/Module-2-Review/Program.cs
+++ Module-2-Review/Module-2-Review/Program.cs
@@ -1,4 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using Module_2_Review;
 
 
+public class Program
+{
+    static void Main()
+    {
+        // create a list of students
+        List<Student> students = new List<Student>();
+        
+        //create and instance of the student class
+        Student student1 = new Student {Name = "Alice", ID = 1 };
+        Student student2 = new Student {Name = "Bob", ID = 2};
+        Student student3 = new Student { Name = "Logan", ID = 3 };
+        Student student4 = new Student { Name = "Madison", ID = 4 };
+        
+        // populate the list
+        students.Add(student1);
+        students.Add(student2);
+        students.Add(student3);
+        students.Add(student4);
+        
+
+    }
+}
